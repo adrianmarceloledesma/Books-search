388f4c783b7f691362c7c6be7af2f46418b042ba
jOh <j0h@example.com>
2020-06-17 22:37:06 -0300

    Updates

---
+++ precache-manifest.1052cfdb7785d4362bf04ef328d9abbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4596cf2a23bf79368a2a55d350fb57",
+    "url": "/adrianmarceloledesma/Books-search/index.html"
+  },
+  {
+    "revision": "8206f1d2b5849ba4f02f",
+    "url": "/adrianmarceloledesma/Books-search/static/css/main.b3810779.chunk.css"
+  },
+  {
+    "revision": "37dc1ac22d884344dab9",
+    "url": "/adrianmarceloledesma/Books-search/static/js/2.227aade9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adrianmarceloledesma/Books-search/static/js/2.227aade9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8206f1d2b5849ba4f02f",
+    "url": "/adrianmarceloledesma/Books-search/static/js/main.e5f3abc8.chunk.js"
+  },
+  {
+    "revision": "cbf26bb3705accb2eafd",
+    "url": "/adrianmarceloledesma/Books-search/static/js/runtime-main.ef86b1fa.js"
+  },
+  {
+    "revision": "8b11f80179b9235a09463c50579b92ab",
+    "url": "/adrianmarceloledesma/Books-search/static/media/img1.8b11f801.png"
+  }
+]);
